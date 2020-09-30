0ae17ac7b0fa3b2c7e3cb4796da56d1f4e08f015
alish-mayor <alish22061999@gmail.com>
2020-10-01 01:55:05 +0600

    feat: problems solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length < 2 || array.length === 0 || array === undefined){
+    return 0
+  }
+  let minim = array[0]
+  for(let i = 0; i<array.length; i++){
+    if(array[i] < minim){
+      minim = array[i]
+    }
+  }
+  return minim;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length < 2 || array.length === 0 || array === undefined){
+    return 0
+  }
+  let maxim = array[0]
+  for(let i = 0; i<array.length; i++){
+    if(array[i] > maxim){
+      maxim = array[i]
+    }
+  }
+  return maxim;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length < 2 || array.length === 0 || array === undefined){
+    return 0
+  }
+  let summ = 0
+  for(let i = 0; i<array.length; i++){
+    summ += array[i]
+  }
+  return summ/array.length;
 }
+
+
+
